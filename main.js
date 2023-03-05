fc25ac789c0c4342e5a0e256e51ca1f2ce5e7457
Mathis <github@mathis.website>
2023-03-05 14:49:17 -0500

    import

---
+++ main.js
@@ -0,0 +1,43 @@
+import './style.css';
+import { WebContainer } from '@webcontainer/api';
+import { files } from './files';
+
+/** @type {import('@webcontainer/api').WebContainer}  */
+let webcontainerInstance;
+
+window.addEventListener('load', async () => {
+  textareaEl.value = files['index.js'].file.contents;
+
+  // Call only once
+  webcontainerInstance = await WebContainer.boot();
+  await webcontainerInstance.mount(files);
+
+  const exitCode = await installDependencies();
+  if (exitCode !== 0) {
+    throw new Error('Installation failed');
+  };
+});
+
+async function installDependencies() {
+  // Install dependencies
+  const installProcess = await webcontainerInstance.spawn('npm', ['install']);
+  // Wait for install command to exit
+  return installProcess.exit;
+}
+
+document.querySelector('#app').innerHTML = `
+  <div class="container">
+    <div class="editor">
+      <textarea>I am a textarea</textarea>
+    </div>
+    <div class="preview">
+      <iframe src="loading.html"></iframe>
+    </div>
+  </div>
+`
+
+/** @type {HTMLIFrameElement | null} */
+const iframeEl = document.querySelector('iframe');
+
+/** @type {HTMLTextAreaElement | null} */
+const textareaEl = document.querySelector('textarea');
